57ee0e442abeb1a06f9422b02149b4ea9d6f212b
jgmarquesm <joaogabrielmarques@discente.ufg.br>
2022-07-24 17:39:06 -0300

    REFACTOR: refatorando o consumo da API

--- APIFIlmes/src/Main.java
+++ APIFIlmes/src/Main.java
@@ -1,43 +1,52 @@
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.List;
-import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception {
 
-        String url = "https://alura-filmes.herokuapp.com/conteudos";
-        URI adress = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(adress).GET().build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String body = response.body();
+        System.out.println("Para usar a API do IMDB, digite: 1.");
+        System.out.println("Para usar a API da Nasa, digite: 2.");
 
-        JsonParser parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        Scanner entrada = new Scanner(System.in);
+        int escolha = entrada.nextInt();
+
+        String url = "";
+        ExtratorDeConteudo extrator = json -> null;
+        String frase = "";
+
+        switch (escolha) {
+            case 1 -> {
+                url = "https://alura-filmes.herokuapp.com/conteudos";
+                extrator = new ExtratorDeConteudoDoIMDB();
+                frase = "FILME TOP";
+            }
+            case 2 -> {
+                url = "https://api.nasa.gov/planetary/apod?api_key=gsaqohWW65PlcpWa4BxLsSWrva0ONy0plFnHKzsi&start_date=2021-11-02&end_date=2022-01-30";
+                extrator = new ExtratorDeConteudoDaNasa();
+                frase = "FOTO TOP";
+            }
+        }
+
+        ImageSearch http = new ImageSearch();
+        String json = http.buscaDados(url);
+
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
 
         StickersMaker maker = new StickersMaker();
 
-        for (Map<String, String> filme : listaDeFilmes) {
+        for (int i = 0; i < 6; i++) {
 
-            String titulo = filme.get("title");
-            String frase = "ESSE É TOP";
-            int endIndex = filme.get("image").indexOf("_");
-            String urlImagem = filme.get("image").substring(0, endIndex - 1) + ".jpg";
+            Conteudo conteudo = conteudos.get(i);
 
-            InputStream inputStream = new URL(urlImagem).openStream();
-            String nomeArquivo = "out/figurinhas/" + titulo + ".png";
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "out/figurinhas/" + conteudo.getTitulo() + ".png";
 
             maker.cria(inputStream, nomeArquivo, frase);
 
-            System.out.println(titulo);
-            System.out.println(urlImagem);
-
+            System.out.println(conteudo.getTitulo());
+            System.out.println();
         }
     }
 }
